1d6b3aa911d32f2e4469a8d946b25189e4de72f7
Jie Ren Tian <110873204@qq.com>
2017-11-15 17:59:38 +0800

    ..

---
+++ php-learning-5.php
@@ -0,0 +1,40 @@
+<html>
+	<body>
+		<form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+			Name: <input type="text" name="name">
+			E-mail: <input type="text" name="email">
+			Gender:
+			<input type="radio" name="gender" value="female">female
+			<input type="radio" name="gender" value="male">male
+			<input type="submit">
+		</form>
+		<?php
+			$name = $email = $gender = "";
+			$nameErr = $emailErr = $genderErr = "";
+
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+				if (empty($_POST['name'])) {
+					$nameErr = 'Name is Required!';
+				} else {
+					$name = format_input($_POST['name']);
+				}
+				
+				$email = format_input($_POST['email']);
+				$gender = format_input($_POST['gender']);
+			}
+
+			function format_input($data) {
+				$data = trim($data);
+				$data = stripcslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+			}
+
+			echo '获取到的值:<br />';
+			echo "$name<br />";
+			echo "$email<br />";
+			echo "$gender<br />";
+		?>
+	</body>
+</html>
