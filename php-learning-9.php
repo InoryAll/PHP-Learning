5845d5c072514621bd48c362f1712ea1d7341d82
Jie Ren Tian <110873204@qq.com>
2017-11-17 14:42:39 +0800

    ..

---
+++ php-learning-9.php
@@ -0,0 +1,12 @@
+<?php
+	echo "今天是".date("Y/m/d").'<br />';
+	echo "今天是".date("Y.m.d").'<br />';
+	echo "今天是".date("Y-m-d").'<br />';
+	echo "今天是".date("l").'<br />';
+
+	date_default_timezone_set("Asia/Shanghai");
+	echo "现在的时间是".date('h:i:sa').'<br />';
+
+	$d = mktime(9, 12, 31, 6, 10, 2015);
+	echo "创建的日期是".date('Y-m-d h:i:sa', $d);
+?>
